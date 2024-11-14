bfa7ac5a528969c4683f07228d8b2a50f96d9974
hanju <rondo2860@gmail.com>
2024-11-14 15:49:48 +0900

    feat: 하이라이팅 된 노드 색 변경

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import './style.css';
 
 cytoscape.use(coseBilkent);
 
-const dimColor = '#dfe4ea';
-const edgeColor = '#ced6e0';
-const nodeColor = '#57606f';
-const nodeActiveColor = '#ffa502';
+//데이터 형식
 
-const successorColor = '#ff6348';
-// 상위 node & edge color
-const predecessorsColor = '#1e90ff';
 const data = [
+    //노드 정의 
     {
         "data": {
             "id": "1",
@@ -28,8 +23,10 @@ const data = [
             "title": "비선형 자료구조"
         }
     },
+
+    // 두 노드 간 엣지 정의 
     {
-        "data": { "id": "ISSUE-packageJson->STUDY-npmInit", "source": "2", "target": "1" }
+        "data": { "id": "test0", "source": "2", "target": "1" }
     },
     {
         "data": {
@@ -165,9 +162,26 @@ const pageRank = cy_for_rank.elements().pageRank();
 
 const nodeMaxSize = 20;
 const nodeMinSize = 5;
+const nodeActiveSize = 20;
 const fontMaxSize = 8;
 const fontMinSize = 5;
+const fontActiveSize = 7;
 
+const edgeWidth = '2px';
+var edgeActiveWidth = '4px';
+const arrowScale = 0.8;
+const arrowActiveScale = 1.2;
+// edge & arrow 크기값
+
+const dimColor = '#e0f7fa';       // 흐린 청록색 - 비활성화된 요소
+const edgeColor = '#b2ebf2';      // 연한 청록색 - 기본 엣지
+const nodeColor = '#00796b';      // 짙은 청록색 - 기본 노드
+const nodeActiveColor = '#00bcd4'; // 밝은 청록색 - 활성화된 노드
+
+const successorColor = '#4fc3f7'; // 후속 노드 및 엣지 색상
+const predecessorsColor = '#0288d1'; // 상위 노드 및 엣지 색상
+
+// 하위 node & edge color
 const cy = cytoscape({
 
     container: document.getElementById('cy'), // container to render in
@@ -195,11 +209,14 @@ const cy = cytoscape({
         {
             selector: 'edge',
             style: {
-                'width': 1,
+              // 변경된 부분
+                'width': edgeWidth,
                 'curve-style': 'bezier',
-                'line-color': '#ccc',
-                'source-arrow-color': '#ccc',
-                'source-arrow-shape': 'vee'
+                'line-color': edgeColor,
+                'source-arrow-color': edgeColor,
+                'source-arrow-shape': 'vee',
+                'arrow-scale': arrowScale
+              //
             }
         }
     ],
@@ -221,8 +238,96 @@ cy.on('tap', function (e) {
 });
 
 
+function setDimStyle(target_cy, style) {
+    target_cy.nodes().forEach(function (target) {
+        target.style(style);
+    });
+    target_cy.edges().forEach(function (target) {
+        target.style(style);
+    });
+}
+
+function setFocus(target_element, successorColor, predecessorsColor, edgeWidth, arrowScale) {
+    target_element.style('background-color', nodeActiveColor);
+    target_element.style('color', nodeColor);
+    target_element.successors().each(function (e) {
+        // 상위  엣지와 노드
+        if (e.isEdge()) {
+            e.style('width', edgeWidth);
+            e.style('arrow-scale', arrowScale);
+        }
+        e.style('color', nodeColor);
+        e.style('background-color', successorColor);
+        e.style('line-color', successorColor);
+        e.style('source-arrow-color', successorColor);
+        setOpacityElement(e, 0.5);
+    }
+    );
+    target_element.predecessors().each(function (e) {
+        // 하위 엣지와 노드
+        if (e.isEdge()) {
+            e.style('width', edgeWidth);
+            e.style('arrow-scale', arrowScale);
+        }
+        e.style('color', nodeColor);
+        e.style('background-color', predecessorsColor);
+        e.style('line-color', predecessorsColor);
+        e.style('source-arrow-color', predecessorsColor);
+        setOpacityElement(e, 0.5);
+    });
+    target_element.neighborhood().each(function (e) {
+        // 이웃한 엣지와 노드
+        setOpacityElement(e, 1);
+    }
+    );
+    target_element.style('width', Math.max(parseFloat(target_element.style('width')), nodeActiveSize));
+    target_element.style('height', Math.max(parseFloat(target_element.style('height')), nodeActiveSize));
+    target_element.style('font-size', Math.max(parseFloat(target_element.style('font-size')), fontActiveSize));
+}
+
+function setOpacityElement(target_element, degree) {
+    target_element.style('opacity', degree);
+}
+
+function setResetFocus(target_cy) {
+    target_cy.nodes().forEach(function (target) {
+        target.style('background-color', nodeColor);
+        var rank = pageRank.rank(target);
+        target.style('width', nodeMaxSize * rank + nodeMinSize);
+        target.style('height', nodeMaxSize * rank + nodeMinSize);
+        target.style('font-size', fontMaxSize * rank + fontMinSize);
+        target.style('color', nodeColor);
+        target.style('opacity', 1);
+    });
+    target_cy.edges().forEach(function (target) {
+        target.style('line-color', edgeColor);
+        target.style('source-arrow-color', edgeColor);
+        target.style('width', edgeWidth);
+        target.style('arrow-scale', arrowScale);
+        target.style('opacity', 1);
+    });
+}
 
 let resizeTimer;
+
+
+//
+
+cy.on('tapstart mouseover', 'node', function (e) {
+    setDimStyle(cy, {
+        'background-color': dimColor,
+        'line-color': dimColor,
+        'source-arrow-color': dimColor,
+        'color': dimColor
+    });
+
+    setFocus(e.target, successorColor, predecessorsColor, edgeActiveWidth, arrowActiveScale);
+});
+
+cy.on('tapend mouseout', 'node', function (e) {
+    setResetFocus(e.cy);
+});
+
 
 window.addEventListener('resize', function () {
     this.clearTimeout(resizeTimer);
@@ -230,4 +335,3 @@ window.addEventListener('resize', function () {
         cy.fit();
     },200);
 });
-//
